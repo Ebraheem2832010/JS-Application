aa0ef4225593ca9633349ef7e3af00a8a77598e6
himo2832010 <himo2832010@gmail.com>
2022-07-27 09:00:33 +0300

    JS1

---
+++ JS1/js/script.js
@@ -0,0 +1,23 @@
+/*
+console.log(" %c Hello World", 'color: blue' )
+
+document.write("<h1>Coding</h1>")
+document.write("<img src='assets/images/picture.jpg' width='100%'")
+document.querySelector('h1').style.color='green';
+document.querySelector('div').style.backgroundColor='pink';
+document.getElementById('ebra').innerHTML="Elias";
+document.getElementById('ebra').style.color="darkgreen";
+*/
+
+var age=12;
+age=13;
+console.log(age);
+let name="Salama";
+name="ali";
+console.log(name);
+const birthDate=28
+console.log(birthDate)
+
+console.log(5+6);
+console.log(20+100);
+console.log(20100*877);
---
+++ JS1/js/script2.js
@@ -0,0 +1,6 @@
+console.log("%c Ebra is the best", 'color: blue');
+var age=13;
+console.log(age);
+document.write("<p>Your Age Is 'age'</p>");
+document.write("<img src=assets/images/background.jpg height=100px>");
+
